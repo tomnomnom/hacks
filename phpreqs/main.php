1b8cd4c32b30866d510c76e186e06347ae107d5d
Tom Hudson <mail@tomnomnom.com>
2017-08-23 01:08:40 +0000

    Auto transport switching for phpreqs

--- phpreqs/main.php
+++ phpreqs/main.php
@@ -1,38 +1,52 @@
 <?php
 
-$urls = [
-    'https://httpbin.org/headers'
+$hosts = [
+    //'https://httpbin.org',
+    'http://httpbin.org'
 ];
 
-$params = [];
+foreach ($hosts as $host) {
+    echo req($host, "/headers", [
+        'Host: httpbin.org',
+        'Origin: null'
+    ]);
+}
 
-foreach ($urls as $url) {
+function req($host, $path = "/", array $headers = []){
 
-    $host = parse_url($url, PHP_URL_HOST);
-    $path = parse_url($url, PHP_URL_PATH);
+    $p = parse_url($host);
+    $host = $p['host'] ?? 'localhost';
+    $port = $p['port'] ?? 80;
+    $scheme = $p['scheme'] ?? 'http';
 
-    $context = stream_context_create($params);
-    $fp = stream_socket_client(
-        "tls://{$host}:443",
-        $errno,
-        $errstr,
-        30,
-        STREAM_CLIENT_CONNECT,
-        $context
-    );
-    if (!$fp) continue;
+    if ($port == 80 && $scheme == 'https'){
+        $port = 443;
+    }
+
+    $transport = "tcp";
+    if ($scheme == 'https'){
+        $transport = 'tls';
+    }
+
+    $fp = stream_socket_client("{$transport}://{$host}:{$port}");
+    if (!$fp) return "";
 
     stream_set_write_buffer($fp, 0);
     stream_set_read_buffer($fp, 0);
 
+    // This needs to support keep-alive rubbish too
+    // But that means parsing the content length header
+    // and... Nah.
     fwrite($fp, "GET {$path} HTTP/1.1\r\n");
-    fwrite($fp, "Host: {$host}\r\n");
     fwrite($fp, "Connection: close\r\n");
-    fwrite($fp, "Origin: {$host}\r\n");
+
+    foreach ($headers as $header){
+        fwrite($fp, "{$header}\r\n");
+    }
     fwrite($fp, "\r\n");
 
     $resp = stream_get_contents($fp);
-
-    echo $resp;
     fclose($fp);
+
+    return $resp;
 }
