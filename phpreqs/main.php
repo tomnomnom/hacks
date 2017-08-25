909fe40c20731f04b0fcfd6074d7eedcb69a6c2c
Tom Hudson <mail@tomnomnom.com>
2017-08-25 12:32:04 +0000

    Adds raw and simple request classes to phpreqs

--- phpreqs/main.php
+++ phpreqs/main.php
@@ -1,87 +1,149 @@
 <?php
 
-$hosts = [
-    'https://httpbin.org',
-    'http://httpbin.org'
-];
 
-foreach ($hosts as $host) {
-    $resp = req($host, "/", [
-        "Host: httpbin.org",
-        "Origin: {$host}"
-    ]);
+class SimpleRequest extends RawRequest {
+    public $method   = 'GET';
+    public $scheme   = 'http';
+    public $path     = '/';
+    public $query    = '';
+    public $fragment = '';
+    public $proto    = 'HTTP/1.1';
 
-    if ($resp->hasHeader('Access-Control-Allow-Origin')){
-        echo "{$host}: ".$resp->getRawHeader('access-control-allow-origin').PHP_EOL;
+    public $headers = [];
+
+    public function __construct($url, array $headers = []){
+        $p = parse_url($url);
+
+        $this->scheme = $p['scheme'] ?? $this->scheme;
+        if (strToLower($this->scheme) == 'https'){
+            $this->port = 443;
+            $this->transport = 'tls';
+        }
+
+        $this->host     = $p['host']     ?? $this->host;
+        $this->port     = $p['port']     ?? $this->port;
+        $this->path     = $p['path']     ?? $this->path;
+        $this->query    = $p['query']    ?? $this->query;
+        $this->fragment = $p['fragment'] ?? $this->fragment;
+
+        $this->addHeader("Host: {$this->host}");
+
+        foreach ($headers as $header){
+            $this->addHeader($header);
+        }
+
     }
 
+    public function addHeader($header){
+        $this->headers[] = $header;
+    }
+
+    public function toString(){
+        $out = "{$this->method} {$this->path} {$this->proto}\r\n";
+        foreach ($this->headers as $header){
+            $out .= "{$header}\r\n";
+        }
+
+        return $out;
+    }
 }
 
+class RawRequest {
+    public $transport = 'tcp';
+    public $host      = 'localhost';
+    public $port      = '80';
+    public $request   = '';
 
-function req($host, $path = "/", array $headers = []){
-
-    $p = parse_url($host);
-    $host = $p['host'] ?? 'localhost';
-    $port = $p['port'] ?? 80;
-    $scheme = $p['scheme'] ?? 'http';
-
-    if ($port == 80 && $scheme == 'https'){
-        $port = 443;
+    public function __construct($raw){
+        $this->request = $raw;
     }
 
-    $transport = "tcp";
-    if ($scheme == 'https'){
-        $transport = 'tls';
+    public function send(){
+        $sock = @stream_socket_client("{$this->transport}://{$this->host}:{$this->port}");
+        if (!$sock) return new \Response(null);
+
+        stream_set_write_buffer($sock, 0);
+        stream_set_read_buffer($sock, 0);
+
+        fwrite($sock, $this->toString());
+        fwrite($sock, "\r\n");
+
+        return new \Response($sock);
     }
 
-    $fp = @stream_socket_client("{$transport}://{$host}:{$port}");
-    if (!$fp) return new \Response("[error]");
-
-    stream_set_write_buffer($fp, 0);
-    stream_set_read_buffer($fp, 0);
-
-    // This needs to support keep-alive rubbish too
-    // But that means parsing the content length header
-    // and... Nah.
-    fwrite($fp, "GET {$path} HTTP/1.1\r\n");
-    fwrite($fp, "Connection: close\r\n");
-
-    foreach ($headers as $header){
-        fwrite($fp, "{$header}\r\n");
+    public function toString(){
+        return $this->request;
     }
-    fwrite($fp, "\r\n");
-
-    $raw = stream_get_contents($fp);
-    fclose($fp);
-
-    return new \Response($raw);
 }
 
 class Response {
-    public $rawStatus = '';
-    public $headers = [];
-    public $body = '';
+    private $socket = null;
+    private $rawStatus = '';
+    private $headers = [];
+    private $body = '';
 
-    public function __construct($raw){
-        $this->parseRaw($raw);
+    public function __construct($socket = null){
+        $this->socket = $socket;
     }
 
-    public function parseRaw($raw){
+    public function readHeaders(){
+        if (!$this->socket) return;
 
-        $lines = explode("\n", $raw);
-        $this->rawStatus = array_shift($lines);
+        // HTTP/1.1 200 OK
+        $status = $this->readLine();
+        if ($status === false){
+            return;
+        }
+        $this->rawStatus = trim($status);
 
-        while ($line = trim(array_shift($lines))){
+        // Content-Type: text/html etc...
+        while (true){
+            $line = $this->readLine();
+            if ($line === false) break;
 
-            if ($line == ''){
-                // Hit the body
-                break;
-            }
+            $line = trim($line);
+            if ($line == "") break;
 
             $this->headers[] = $line;
         }
+    }
 
-        $this->body = implode("\n", $lines);
+    // Read the body from the response using either the Content-Length header to
+    // specify how many bytes to read, or just read until the connection closes.
+    public function readBody(){
+        if ($this->hasHeader('Content-Length')){
+            $this->body = $this->readBytes((int) $this->getHeader('Content-Length'));
+        } else {
+            $this->body = $this->readUntilClosed();
+        }
+    }
+
+    private function readLine(){
+        if (!$this->socket) return false;
+        return fgets($this->socket);
+    }
+
+    private function readBytes($count){
+        if (!$this->socket) return false;
+        if ($count <= 0) return false;
+        return fread($this->socket, $count);
+    }
+
+    private function readUntilClosed(){
+        $out = '';
+        while (true){
+            $line = $this->readLine();
+            if ($line === false) break;
+
+            $out .= $line;
+        }
+
+        return $out;
+    }
+
+    public function close(){
+        if (!$this->socket) return false;
+        fclose($this->socket);
     }
 
     public function getRawHeader($search){
@@ -121,15 +183,31 @@ class Response {
         return strToLower(trim($str));
     }
 
+    public function toString(){
+        $out = "{$this->rawStatus}\r\n";
+
+        foreach ($this->headers as $header){
+            $out .= "{$header}\r\n";
+        }
+
+        $out .= "\r\n";
+        $out .= $this->body;
+
+        return $out;
+    }
+
 }
 
 
+$req = new SimpleRequest("http://httpbin.org/headers", [
+    'Origin: http://example.com'
+]);
+$resp = $req->send();
+$resp->readHeaders();
+$resp->readBody();
+$resp->close();
 
-
-
-
-
-
-
-
+echo $req->toString();
+echo PHP_EOL;
+echo $resp->toString();
 
