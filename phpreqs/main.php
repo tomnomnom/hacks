83078c5ceb44b2df009a9e9cd47128e534becd43
Tom Hudson <mail@tomnomnom.com>
2017-08-25 15:11:36 +0000

    Adds example output

--- phpreqs/main.php
+++ phpreqs/main.php
@@ -20,3 +20,43 @@ echo $req->toString();
 echo PHP_EOL;
 echo $resp->toString();
 
+/*
+E.g. output:
+
+POST /anything?param1=value1&param2=value2#thefragment HTTP/1.1
+Host: httpbin.org
+Origin: http://evil.com
+
+HTTP/1.1 200 OK
+Connection: keep-alive
+Server: meinheld/0.6.1
+Date: Fri, 25 Aug 2017 15:10:22 GMT
+Content-Type: application/json
+Access-Control-Allow-Origin: http://evil.com
+Access-Control-Allow-Credentials: true
+X-Powered-By: Flask
+X-Processed-Time: 0.00148296356201
+Content-Length: 388
+Via: 1.1 vegur
+
+{
+  "args": {
+    "param1": "value1", 
+    "param2": "value2"
+  }, 
+  "data": "", 
+  "files": {}, 
+  "form": {}, 
+  "headers": {
+    "Connection": "close", 
+    "Content-Length": "0", 
+    "Host": "httpbin.org", 
+    "Origin": "http://evil.com"
+  }, 
+  "json": null, 
+  "method": "POST", 
+  "origin": "123.123.123.123", 
+  "url": "http://httpbin.org/anything?param1=value1&param2=value2"
+}
+
+*/
