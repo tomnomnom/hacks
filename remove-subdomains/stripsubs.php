290cf72a95bddfb7c395b5a2b14a20d666717b6f
Tom Hudson <mail@tomnomnom.com>
2017-08-29 18:43:09 +0000

    Adds stripsubs script

---
+++ remove-subdomains/stripsubs.php
@@ -0,0 +1,38 @@
+#!/usr/bin/env php
+<?php
+$suffixes = @file('suffixes.txt');
+if (!$suffixes) die('failed to load suffixes.txt');
+
+$suffixes = array_map(function($line){
+    return trim($line, " \t\n\r\0\x0B.");
+}, $suffixes);
+
+$suffixes = array_filter($suffixes, function($line){
+    return $line != "";
+});
+
+$map = [];
+
+foreach ($suffixes as $suffix){
+   $map[$suffix] = true; 
+}
+
+while (($line = fgets(STDIN)) != false){
+    $candidate = trim($line);
+    $parts = explode(".", $candidate);
+
+    $tail = [];
+    array_unshift($tail, array_pop($parts));
+
+    if (isset($map[$tail[0]])){
+        // We've got a tld, try for an sld
+        array_unshift($tail, array_pop($parts));
+
+        if (!isset($map[implode(".", $tail)])){
+            // No match, put it back
+            array_push($parts, array_shift($tail));
+        }
+    }
+
+    echo array_pop($parts).".".implode(".", $tail).PHP_EOL;
+}
