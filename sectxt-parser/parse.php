d8f885f80521efe4de254796536f0960183423ff
Tom Hudson <mail@tomnomnom.com>
2017-08-31 19:44:22 +0000

    Adds helper methods etc to sectxt parser

--- sectxt-parser/parse.php
+++ sectxt-parser/parse.php
@@ -23,6 +23,12 @@ class SecurityTxt {
         self::FIELD_ACKNOWLEDGEMENT => [],
     ];
 
+    public function __construct($raw = ""){
+        if ($raw){
+            $this->parse($raw);
+        }
+    }
+
     public function parse($raw){
         $lines = explode("\n", $raw);
 
@@ -54,40 +60,8 @@ class SecurityTxt {
             $option = strToLower($parts[0]);
             $value = trim($parts[1]);
 
-            switch ($option){
-                case self::FIELD_CONTACT:
-                    $lower = strToLower($value);
-                        if (!(
-                            filter_var($value, FILTER_VALIDATE_URL) ||
-                            filter_var($value, FILTER_VALIDATE_EMAIL) ||
-                            $this->validatePhone($value)
-                        )){
-                            $this->addError("invalid value '{$value}' for option '{$parts[0]}' on line {$n}");
-                            continue 2;
-                        }
-
-                    break;
-
-                case self::FIELD_DISCLOSURE:
-                    if (!in_array(strToLower($value), $this->validDisclosures)){
-                        $this->addError("invalid value '{$value}' for option '{$parts[0]}' on line {$n}; must be one of [".implode(", ", $this->validDisclosures)."]");
-                        continue 2;
-                    }
-                    break;
-
-                case self::FIELD_ENCRYPTION:
-                case self::FIELD_ACKNOWLEDGEMENT:
-                    if (!filter_var($value, FILTER_VALIDATE_URL)){
-                        $this->addError("invalid URI '{$value}' for option '{$parts[0]}' on line {$n}");
-                        continue 2;
-                    }
-                    break;
-
-
-                default:
-                    $this->addError("invalid option '{$parts[0]}' on line {$n}");
-                    continue 2;
-                    break;
+            if (!$this->validateField($option, $value, $n)){
+                continue;
             }
 
             $this->fields[$option][] = $value;
@@ -99,12 +73,55 @@ class SecurityTxt {
 
     }
 
-    public function comments(){
-        return $this->comments;
+    private function validateField($option, $value, $lineNo = 0){
+        switch ($option){
+            case self::FIELD_CONTACT:
+                return $this->validateContact($option, $value, $lineNo);
+
+            case self::FIELD_DISCLOSURE:
+                return $this->validateDisclosure($option, $value, $lineNo);
+
+            case self::FIELD_ENCRYPTION:
+            case self::FIELD_ACKNOWLEDGEMENT:
+                return $this->validateUri($option, $value, $lineNo);
+
+            default:
+                $this->addError("invalid option '{$option}' on line {$lineNo}");
+        }
+        return false;
     }
 
-    public function fields(){
-        return $this->fields;
+    private function validateContact($option, $value, $lineNo){
+        $lower = strToLower($value);
+        if (!(
+            filter_var($value, FILTER_VALIDATE_URL) ||
+            filter_var($value, FILTER_VALIDATE_EMAIL) ||
+            $this->isValidPhoneNumber($value)
+        )){
+            $this->addError("invalid value '{$value}' for option '{$option}' on line {$lineNo}");
+            return false;
+        }
+        return true;
+    }
+
+    private function validateDisclosure($option, $value, $lineNo){
+        if (!in_array(strToLower($value), $this->validDisclosures)){
+            $this->addError("invalid value '{$value}' for option '{$option}' on line {$lineNo}; must be one of [".implode(", ", $this->validDisclosures)."]");
+            return false;
+        }
+        return true;
+    }
+
+    private function validateUri($option, $value, $lineNo){
+        if (!filter_var($value, FILTER_VALIDATE_URL)){
+            $this->addError("invalid URI '{$value}' for option '{$option}' on line {$lineNo}");
+            return false;
+        }
+        return true;
+    }
+
+    private function isValidPhoneNumber($candidate){
+        return (preg_match("/^\+[0-9\(\) -]+$/", $candidate) > 0);
     }
 
     private function addError($msg){
@@ -119,8 +136,48 @@ class SecurityTxt {
         return $this->errors;
     }
 
-    private function validatePhone($candidate){
-        return (preg_match("/^\+[0-9\(\) -]+$/", $candidate) > 0);
+    public function hasComments(){
+        return (sizeOf($this->comments) > 0);
+    }
+
+    public function comments(){
+        return $this->comments;
+    }
+
+    public function hasContact(){
+        return (sizeOf($this->fields[self::FIELD_CONTACT]) > 0);
+    }
+
+    public function contact(){
+        return $this->fields[self::FIELD_CONTACT];
+    }
+
+    public function hasDisclosure(){
+        return (sizeOf($this->fields[self::FIELD_DISCLOSURE]) > 0);
+    }
+
+    public function disclosure(){
+        return $this->fields[self::FIELD_DISCLOSURE];
+    }
+
+    public function hasEncryption(){
+        return (sizeOf($this->fields[self::FIELD_ENCRYPTION]) > 0);
+    }
+
+    public function encryption(){
+        return $this->fields[self::FIELD_ENCRYPTION];
+    }
+
+    public function hasAcknowledgement(){
+        return (sizeOf($this->fields[self::FIELD_ACKNOWLEDGEMENT]) > 0);
+    }
+
+    public function acknowledgement(){
+        return $this->fields[self::FIELD_ACKNOWLEDGEMENT];
+    }
+
+    public function fields(){
+        return $this->fields;
     }
 
 }
