643387a681e09e5bd35fa941531140da210d94ca
Tom Hudson <mail@tomnomnom.com>
2017-12-23 17:31:59 +0000

    Adds waybackurls fetcher

---
+++ waybackurls/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+const fetchURL = "http://web.archive.org/cdx/search/cdx?url=*.%s/*&output=json&fl=original&collapse=urlkey"
+
+func main() {
+
+	sc := bufio.NewScanner(os.Stdin)
+
+	for sc.Scan() {
+		domain := sc.Text()
+
+		urls, err := getWaybackURLs(domain)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to fetch URLs for [%s]\n", domain)
+			continue
+		}
+
+		for _, url := range urls {
+			fmt.Println(url)
+		}
+	}
+
+	if err := sc.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "failed to read input: %s\n", err)
+	}
+
+}
+
+func getWaybackURLs(domain string) ([]string, error) {
+
+	out := make([]string, 0)
+
+	res, err := http.Get(fmt.Sprintf(fetchURL, domain))
+	if err != nil {
+		return out, err
+	}
+
+	raw, err := ioutil.ReadAll(res.Body)
+
+	res.Body.Close()
+	if err != nil {
+		return out, err
+	}
+
+	var wrapper [][]string
+	err = json.Unmarshal(raw, &wrapper)
+
+	skip := true
+	for _, urls := range wrapper {
+		// The first item is always just the string "original",
+		// so we should skip the first item
+		if skip {
+			skip = false
+			continue
+		}
+		out = append(out, urls...)
+	}
+
+	return out, nil
+
+}
