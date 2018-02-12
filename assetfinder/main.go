7cdd94ef536ab6dd56909af3002b2ac23c6c7288
Tom Hudson <mail@tomnomnom.com>
2018-02-12 22:47:38 +0000

    Adds threatcrowd

--- assetfinder/main.go
+++ assetfinder/main.go
@@ -23,10 +23,14 @@ func main() {
 
 	// TODO:
 	//   https://crt.sh/?q=%25.%s&output=json
-	//   https://www.threatcrowd.org/searchApi/v2/domain/report/?domain=%s
+	//   crt.sh's return format isn't actually valid JSON;
+	//   it's a series of JSON objects with no delimiter
+	//   between them. I mean seriously, why would you
+	//   do that?!
 	sources := []fetchFn{
 		fetchCertSpotter,
 		fetchHackerTarget,
+		fetchThreatCrowd,
 	}
 
 	// track what we've already printed to avoid duplicates
@@ -39,6 +43,7 @@ func main() {
 		}
 
 		for _, n := range names {
+			n = cleanDomain(n)
 			if _, ok := printed[n]; ok {
 				continue
 			}
@@ -49,6 +54,29 @@ func main() {
 }
 
 type fetchFn func(string) ([]string, error)
+
+func fetchThreatCrowd(domain string) ([]string, error) {
+	out := make([]string, 0)
+
+	raw, err := httpGet(
+		fmt.Sprintf("https://www.threatcrowd.org/searchApi/v2/domain/report/?domain=%s", domain),
+	)
+	if err != nil {
+		return out, err
+	}
+
+	wrapper := struct {
+		Subdomains []string `json:"subdomains"`
+	}{}
+	err = json.Unmarshal(raw, &wrapper)
+	if err != nil {
+		return out, err
+	}
+
+	out = append(out, wrapper.Subdomains...)
+
+	return out, nil
+}
 
 func fetchHackerTarget(domain string) ([]string, error) {
 	out := make([]string, 0)
@@ -112,4 +140,22 @@ func httpGet(url string) ([]byte, error) {
 	}
 
 	return raw, nil
+}
+
+func cleanDomain(d string) string {
+	// no idea what this is, but we can't clean it ¯\_(ツ)_/¯
+	if len(d) < 2 {
+		return d
+	}
+
+	if d[0] == '*' || d[0] == '%' {
+		d = d[1:]
+	}
+
+	if d[0] == '.' {
+		d = d[1:]
+	}
+
+	return d
+
 }
