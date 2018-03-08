5f9acfafe80b416e859eb2a488b876805b30cabf
Tom Hudson <mail@tomnomnom.com>
2018-03-08 00:57:45 +0000

    Adds chromeredir

---
+++ chromeredir/checkredir.js
@@ -0,0 +1,22 @@
+const puppeteer = require('puppeteer');
+
+if (process.argv[2] == "") {
+    console.log("usage: node checkredir.js <URL>")
+    process.exit(1);
+}
+
+url = process.argv[2];
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  const finalLocation = await page.evaluate(() => {
+    return document.location.href;
+  });
+
+  console.log('Final URL:', finalLocation);
+
+  await browser.close();
+})();
