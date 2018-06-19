6e679555aaaf1d5dfa0599b9892c97713af38861
Tom Hudson <mail@tomnomnom.com>
2018-06-19 10:39:36 +0000

    Initial version of customer parser

--- jsstrings/main.go
+++ jsstrings/main.go
@@ -3,26 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
+	"io/ioutil"
+	"os"
+	"unicode/utf8"
 
-	"github.com/robertkrimen/otto/ast"
-	"github.com/robertkrimen/otto/parser"
+	"github.com/tomnomnom/rplex"
 )
-
-type stringFinder struct {
-}
-
-func (w *stringFinder) Enter(n ast.Node) ast.Visitor {
-
-	if id, ok := n.(*ast.StringLiteral); ok && id != nil {
-		fmt.Println(id.Value)
-	}
-
-	return w
-}
-
-func (w *stringFinder) Exit(n ast.Node) {
-}
 
 func main() {
 	flag.Parse()
@@ -33,12 +19,137 @@ func main() {
 		return
 	}
 
-	program, err := parser.ParseFile(nil, fn, nil, 0)
+	f, err := os.Open(fn)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		return
 	}
 
-	w := &stringFinder{}
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	ast.Walk(w, program)
+	s := string(b)
+
+	lex := rplex.New(s)
+	tokens := lex.Run(lexText)
+
+	for _, t := range tokens {
+		if t.Text() == "" {
+			continue
+		}
+		fmt.Printf("%s\n", t.Text())
+	}
+}
+
+type stringLiteral struct {
+	rplex.TextToken
+}
+
+type comment struct {
+	rplex.TextToken
+}
+
+func lexText(l *rplex.Lexer) rplex.LexFn {
+	l.AcceptUntil("'\"`/")
+	l.Ignore()
+	r := l.Peek()
+
+	switch r {
+	case '"':
+		return lexDouble
+	case '\'':
+		return lexSingle
+	case '`':
+		return lexBacktick
+	case '/':
+		return lexComment
+	default:
+		return nil
+	}
+
+	return nil
+}
+
+func lexDouble(l *rplex.Lexer) rplex.LexFn {
+	l.Accept("\"")
+	l.Ignore()
+
+	l.AcceptUntilUnescaped("\"")
+	l.Emit(&stringLiteral{})
+
+	l.Accept("\"")
+	l.Ignore()
+
+	return lexText
+}
+
+func lexSingle(l *rplex.Lexer) rplex.LexFn {
+	l.Accept("'")
+	l.Ignore()
+
+	l.AcceptUntilUnescaped("'")
+	l.Emit(&stringLiteral{})
+
+	l.Accept("'")
+	l.Ignore()
+
+	return lexText
+}
+
+func lexBacktick(l *rplex.Lexer) rplex.LexFn {
+	l.Accept("`")
+	l.Ignore()
+
+	l.AcceptUntilUnescaped("`")
+	l.Emit(&stringLiteral{})
+
+	l.Accept("`")
+	l.Ignore()
+
+	return lexText
+}
+
+func lexComment(l *rplex.Lexer) rplex.LexFn {
+	l.Accept("/")
+
+	line := l.Accept("/")
+	block := l.Accept("*")
+	l.Ignore()
+
+	if !line && !block {
+		// false alarm; division or a regex literal
+		return lexToEndOfStatement
+	}
+
+	if line {
+		l.AcceptUntil("\n")
+		l.Emit(&comment{})
+		return lexText
+	}
+
+	// read until we hit '*/'
+	for {
+		if l.Cur == utf8.RuneError {
+			return nil
+		}
+		l.AcceptUntil("*")
+		l.Accept("*")
+		if l.Peek() == '/' {
+			l.Accept("/")
+			l.Emit(&comment{})
+			break
+		}
+	}
+
+	return lexText
+}
+
+func lexToEndOfStatement(l *rplex.Lexer) rplex.LexFn {
+	l.AcceptUntilUnescaped(";\n")
+	l.Accept(";\n")
+	l.Ignore()
+	return lexText
 }
