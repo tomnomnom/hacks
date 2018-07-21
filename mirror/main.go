7a95ecafcd90eafcb489220ae04a80a3aeb8cab0
Tom Hudson <mail@tomnomnom.com>
2018-07-21 12:35:06 +0000

    Adds ability to read urls from stdin to mirror

--- mirror/main.go
+++ mirror/main.go
@@ -1,60 +1,67 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
 	"regexp"
+	"strings"
 )
 
 func main() {
 	flag.Parse()
 
-	rawURL := flag.Arg(0)
-	if rawURL == "" {
-		fmt.Println("usage: mirror <url>")
-		return
+	var input io.Reader
+	input = strings.NewReader(flag.Arg(0))
+	if flag.Arg(0) == "" {
+		input = os.Stdin
 	}
 
-	u, err := url.Parse(rawURL)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	sc := bufio.NewScanner(input)
 
-	resp, err := http.Get(rawURL)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	defer resp.Body.Close()
-
-	b, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	body := string(b)
-
-	for k, vv := range u.Query() {
-		for _, v := range vv {
-
-			// a fairly shonky way to get a few chars of context either side of the match
-			re, err := regexp.Compile("(.{0,6}" + regexp.QuoteMeta(v) + ".{0,6})")
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "regexp compile error: %s", err)
-			}
-
-			matches := re.FindAllStringSubmatch(body, -1)
-
-			for _, m := range matches {
-				fmt.Printf("query string key '%s' with value '%s' reflected in response body (...%s...)\n", k, v, m[0])
-			}
+	for sc.Scan() {
+		u, err := url.Parse(sc.Text())
+		if err != nil {
+			fmt.Println(err)
+			return
 		}
 
+		resp, err := http.Get(u.String())
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		defer resp.Body.Close()
+
+		b, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		body := string(b)
+
+		for k, vv := range u.Query() {
+			for _, v := range vv {
+
+				// a fairly shonky way to get a few chars of context either side of the match
+				re, err := regexp.Compile("(.{0,6}" + regexp.QuoteMeta(v) + ".{0,6})")
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "regexp compile error: %s", err)
+				}
+
+				matches := re.FindAllStringSubmatch(body, -1)
+
+				for _, m := range matches {
+					fmt.Printf("query string key '%s' with value '%s' reflected in response body (...%s...)\n", k, v, m[0])
+				}
+			}
+
+		}
 	}
 }
