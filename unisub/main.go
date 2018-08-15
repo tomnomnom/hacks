e3743bf952b73e143fb0141a1e87de8958671993
Tom Hudson <mail@tomnomnom.com>
2018-08-15 15:48:30 +0000

    Adds toLower and toUpper checks to unisub

--- unisub/main.go
+++ unisub/main.go
@@ -4,13 +4,14 @@ import (
 	"flag"
 	"fmt"
 	"net/url"
+	"strings"
 )
 
 func main() {
 	flag.Parse()
 
 	char := flag.Arg(0)
-	if len(char) != 1 {
+	if flag.NArg() < 1 {
 		fmt.Println("usage: unisub <char>")
 		return
 	}
@@ -22,6 +23,21 @@ func main() {
 	}
 
 	for _, s := range subs {
-		fmt.Printf("%c %U %s\n", s, s, url.QueryEscape(string(s)))
+		fmt.Printf("fallback: %c %U %s\n", s, s, url.QueryEscape(string(s)))
+	}
+
+	for cp := 1; cp < 0x10FFFF; cp++ {
+		s := rune(cp)
+		if char == string(s) {
+			continue
+		}
+
+		if strings.ToLower(string(s)) == char {
+			fmt.Printf("toLower: %c %U %s\n", s, s, url.QueryEscape(string(s)))
+		}
+
+		if strings.ToUpper(string(s)) == char {
+			fmt.Printf("toUpper: %c %U %s\n", s, s, url.QueryEscape(string(s)))
+		}
 	}
 }
