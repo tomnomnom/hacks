e4efd4c278fdc084e8ed26459626b297b017c2e0
Tom Hudson <mail@tomnomnom.com>
2019-07-19 22:22:47 +0000

    webpaste writes to stdout

--- webpaste/main.go
+++ webpaste/main.go
@@ -3,24 +3,22 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
-	"path/filepath"
 )
 
 type payload struct {
 	Token string `json:"token"`
-	File  string `json:"file"`
 	Data  string `json:"data"`
 }
 
 func payloadHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	w.Header().Set("Content-Type", "text/plain")
 
 	if r.Method != "POST" {
-		http.Error(w, "no.", http.StatusMethodNotAllowed)
+		fmt.Fprint(w, "not gonna happen sorry")
 		return
 	}
 
@@ -39,28 +37,10 @@ func payloadHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// base name only plz
-	p.File = filepath.Base(p.File)
-
-	f, err := os.OpenFile(p.File, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	_, err = f.WriteString(p.Data + "\n")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	log.Printf("wrote to %s\n", p.File)
-
-	fmt.Sprint(w, "did it\n")
+	fmt.Println(p.Data)
 }
 
 func main() {
 	http.HandleFunc("/", payloadHandler)
-
-	log.Println("Go!")
 	http.ListenAndServe(":8443", nil)
 }
