bf5a960adfca3df60ebf825d42bb673a931a1786
Tom Hudson <mail@tomnomnom.com>
2020-02-19 22:17:19 +0000

    Refactors kxss into two worker pools

--- kxss/main.go
+++ kxss/main.go
@@ -8,35 +8,78 @@ import (
 	"net/url"
 	"os"
 	"strings"
+	"sync"
 )
+
+type appendCheck struct {
+	url   string
+	param string
+}
 
 func main() {
 
 	sc := bufio.NewScanner(os.Stdin)
 
-	for sc.Scan() {
-		inputURL := sc.Text()
-		reflected, err := checkReflected(inputURL)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "error from checkReflected: %s\n", err)
-		}
+	initialChecks := make(chan string, 10)
+	appendChecks := make(chan appendCheck, 10)
 
-		if len(reflected) == 0 {
-			fmt.Printf("no params were reflected in %s\n", inputURL)
-			continue
-		}
+	// initial check worker pool
+	var wgInitial sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wgInitial.Add(1)
+		go func() {
+			for inputURL := range initialChecks {
 
-		for _, r := range reflected {
-			worked, err := checkAppend(inputURL, r)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "error requesting %s with modified %s param: %s", inputURL, r, err)
-				continue
+				reflected, err := checkReflected(inputURL)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "error from checkReflected: %s\n", err)
+					continue
+				}
+
+				if len(reflected) == 0 {
+					fmt.Printf("no params were reflected in %s\n", inputURL)
+					continue
+				}
+
+				for _, param := range reflected {
+					appendChecks <- appendCheck{inputURL, param}
+				}
+
 			}
-			if worked {
-				fmt.Printf("got reflection of param '%s' on %s\n", r, inputURL)
-			}
-		}
+			wgInitial.Done()
+		}()
 	}
+
+	// append check worker pool
+	var wgAppend sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wgAppend.Add(1)
+
+		go func() {
+			for c := range appendChecks {
+				wasReflected, err := checkAppend(c.url, c.param)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "error from checkAppend for url %s with param %s: %s", c.url, c.param, err)
+					continue
+				}
+
+				if wasReflected {
+					fmt.Printf("got reflection of appended param %s on %s\n", c.param, c.url)
+				}
+			}
+			wgAppend.Done()
+		}()
+
+	}
+
+	for sc.Scan() {
+		initialChecks <- sc.Text()
+	}
+
+	close(initialChecks)
+	wgInitial.Wait()
+	close(appendChecks)
+	wgAppend.Wait()
 
 }
 
@@ -84,12 +127,14 @@ func checkAppend(targetURL, param string) (bool, error) {
 		return false, err
 	}
 
-	val := u.Query().Get(param)
+	qs := u.Query()
+	val := qs.Get(param)
 	if val == "" {
 		return false, fmt.Errorf("can't append to non-existant param %s", param)
 	}
 
-	u.Query().Set(param, val+"lol this should be a random string")
+	qs.Set(param, val+"lol this should be a random string")
+	u.RawQuery = qs.Encode()
 
 	reflected, err := checkReflected(u.String())
 	if err != nil {
